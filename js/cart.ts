e7e19f22786ca05238db557b0d19e35d356108e2
Chizalam <cemuchay@gmail.com>
2022-05-17 21:22:00 +0100

    .

--- js/cart.ts
+++ js/cart.ts
@@ -5,80 +5,80 @@ const hatDetailsa = [
     {
         name: "hat1",
         price: "100",
-        img: "img/hat1.jpg",
+        img: "/hat1.jpg",
         description: "hat1 description",
     },
     {
         name: "hat2",
         price: "200",
-        img: "img/hat2.jpg",
+        img: "/hat2.jpg",
         description: "hat2 description"
     },
     {
         name: "hat3",
         price: "300",
-        img: "img/hat3.jpg",
+        img: "/hat3.jpg",
         description: "hat3 description"
     },
     {
         name: "hat4",
         price: "400",
-        img: "img/hat4.jpg",
+        img: "/hat4.jpg",
         description: "hat4 description"
     },
 
     {
         name: "hat5",
         price: "500",
-        img: "img/hat5.jpg",
+        img: "/hat5.jpg",
         description: "hat5 description"
     },
     {
         name: "hat6",
         price: "600",
-        img: "img/hat6.jpg",
+        img: "/hat6.jpg",
         description: "hat6 description"
     },
     {
         name: "hat7",
         price: "700",
-        img: "img/hat7.jpg",
+        img: "/hat7.jpg",
         description: "hat7 description"
     },
     {
         name: "hat8",
         price: "800",
-        img: "img/hat8.jpg",
+        img: "/hat8.jpg",
         description: "hat8 description"
     },
     {
         name: "hat9",
         price: "900",
-        img: "img/hat9.jpg",
+        img: "/hat9.jpg",
         description: "hat9 description"
     },
     {
         name: "hat10",
         price: "1000",
-        img: "img/hat10.jpg",
+        img: "/hat10.jpg",
         description: "hat10 description"
     },
     {
         name: "hat11",
         price: "1100",
-        img: "img/hat11.jpg",
+        img: "/hat11.jpg",
         description: "hat11 description"
     },
     {
         name: "hat12",
         price: "1200",
-        img: "img/hat12.jpg",
+        img: "/hat12.jpg",
         description: "hat12 description"
     },
     {
         name: "hat13",
         price: "1300",
-        img: "img/hat13.jpg",
+        img: "/hat13.jpg",
         description: "hat13 description"
     }
 ]
@@ -102,7 +102,19 @@ for (let i = 0; i < myItemsArray.length; i++) {
     }
 }
 
-console.log(myItemsArrayWithDetails);
+//get data from myitemsarraywithdetails and build checkout page
+let checkoutPage = document.querySelector("#checkoutPage");
+
+for (let i = 0; i < myItemsArrayWithDetails.length; i++) {
+
+    let checkoutPageItem = document.createElement("div");
+    checkoutPage.append(checkoutPageItem);
 
 
+    let checkoutPageItemImg = document.createElement("img");
+
+    checkoutPageItemImg.src = `./images/gallery/${myItemsArrayWithDetails[i].img}`;
+    checkoutPageItem.append(checkoutPageItemImg);
+
+}
 
